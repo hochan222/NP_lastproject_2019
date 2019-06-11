74997dcd1fc1a447f22cda09c2ce1084427ae683
hochan222 <201502766@hufs.ac.kr>
2019-06-12 01:53:19 +0900

    add slack

--- backend/utils/django - 복사본.py
+++ backend/utils/django - 복사본.py
@@ -4,45 +4,6 @@ import logging
 import threading
 import time
 
-from slacker import Slacker
-import os, json
-from django.core.exceptions import ImproperlyConfigured
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TOKEN = []
-secret_file = os.path.join(BASE_DIR, 'secret', 'slack_token.json')
-
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets):
-    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
-    try:
-        return secrets["slackToken"]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-def slack_notify(text=None, channel='#backend', username='알림봇', attachments=None):
-    # TOKEN = get_secret("slackToken")
-    TOKEN = "xoxb-623136227107-661697401447-LGhp25lqduy8VLqWzZ5z1lPA"
-    slack = Slacker(TOKEN)
-    attachments = [{
-    "pretext": "경고 알림",
-    # "color": "#36a64f",
-    "color": "#ed2939",
-    "title": "누군가가 노트북에 손을 댔습니다.",
-    "title_link": "http://127.0.0.1:8000/img/",
-    "fallback": "클라이언트에서 노티피케이션에 보이는 텍스트 입니다. attachment 블록에는 나타나지 않습니다",
-    # "image_url": "http://127.0.0.1:8000/img/",
-    "text": "자세히 보기 대충 이런 내용입니다.",
-    "mrkdwn_in": ["text", "pretext"],
-    }]
-    # "text": "{}".format(book.title)
-
-    slack.chat.post_message(text=text, channel=channel, username=username, attachments=attachments)
-
-
 # main: 10007, rssi_renew_thread: 8090
 # raspberry: 8095
 
@@ -90,11 +51,11 @@ class IoTRequestHandler(socketserver.StreamRequestHandler):
             activate = {}
             if rssi:    # both the temperature and humidity reported
                 # activate actuators if necessary to control
-                if (rssi < 54 ) :
+                if (rssi < 60 ) :
                     activate['buzzer'] = 'ON'
                     buzzer_state = "ON"
-                    slack_notify()
-                elif (rssi >= 54):
+
+                elif (rssi >= 60):
                     activate['buzzer'] = 'OFF'
                     buzzer_state = "OFF"
 
