6aa6ebff95105b542b615c904bd6709013480fda
hochan222 <201502766@hufs.ac.kr>
2019-06-11 22:55:09 +0900

    hmm add 3server

--- backend/utils/django.py
+++ backend/utils/django.py
@@ -8,10 +8,13 @@ import time
 # raspberry: 8095
 
 rssi = 0
-
+buzzer_state = None
 # django main server
 class IoTRequestHandler(socketserver.StreamRequestHandler):
     def handle(self):
+        global rssi
+        global buzzer_state
+
         client = self.request.getpeername()
         logging.info("Client connecting: {}".format(client))
 
@@ -50,15 +53,12 @@ class IoTRequestHandler(socketserver.StreamRequestHandler):
                 # activate actuators if necessary to control
                 if (rssi > 50 ) :
                     activate['buzzer'] = 'ON'
-                    
-                    # django >>> raspberry socket
-                    client = raspberryIoTClient(("127.0.0.1", 8095), 99, 'ON')
-                    client.run()
+                    buzzer_state = "ON"
+
                 elif (rssi <= 50):
                     activate['buzzer'] = 'OFF'
+                    buzzer_state = "OFF"
 
-                    client = raspberryIoTClient(("127.0.0.1", 8095), 99, 'OFF')
-                    client.run()
                 else:
                     pass        # nothing to activate actuators
 
@@ -78,6 +78,8 @@ class IoTRequestHandler(socketserver.StreamRequestHandler):
 # rssi renew server
 class rssiRenew(socketserver.StreamRequestHandler):
     def handle(self):
+        global rssi
+
         client = self.request.getpeername()
         logging.info("Client connecting: {}".format(client))
 
@@ -117,84 +119,72 @@ class rssiRenew(socketserver.StreamRequestHandler):
         # end of for loop
         logging.info('Client closing: {}'.format(client))
 
-
-
-
-
-
-
-
-# raspberry pi buzzer
-class raspberryIoTClient:
-    def __init__(self, server_addr, deviceid, status_of):
-        """IoT client with persistent connection
-        Each message separated by b'\n'
-
-        :param server_addr: (host, port)
-        :param deviceid: id of this IoT
-        """
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(server_addr)  # connect to server process
-        rfile = sock.makefile('rb')  # file-like obj
-        sel = selectors.DefaultSelector()
-        sel.register(sock, selectors.EVENT_READ)
-
-        self.sock = sock
-        self.rfile = rfile
-        self.deviceid = deviceid
-        self.sel = sel
-        self.requests = {}      # messages sent but not yet received their responses
-        self.time_to_expire = None
-        self.status = status_of
-
-    def run(self):
-        # Report sensors' data forever
-        msgid = 0
-
-        try:      # timeout occurs
-            data = dict(buzzer=self.status)
-            # msgid = str(uuid.uuid1())
-            msgid += 1
-            request = dict(method='POST', deviceid=self.deviceid, msgid=msgid, data=data)
-            logging.debug(request)
-            request_bytes = json.dumps(request).encode('utf-8') + b'\n'
-            self.sock.sendall(request_bytes)
-            self.requests[msgid] = request_bytes
-        except:               # EVENT_READ
-            response_bytes = self.rfile.readline()     # receive response
-            if not response_bytes:
-                self.sock.close()
-                raise OSError('Server abnormally terminated')
-            response = json.loads(response_bytes.decode('utf-8'))
-            logging.debug(response)
-
-            # msgid in response allows to identify the specific request message
-            # It enables asynchronous transmission of request messages in pipelining
-            msgid = response.get('msgid')
-            if msgid and msgid in self.requests:
-                del self.requests[msgid]
-            else:
-                logging.warning('{}: illegal msgid received. Ignored'.format(msgid))
-     
-        # end of while loop
-
-        logging.info('raspberry client terminated')
-        self.sock.close()
-
-
 # logging.basicConfig(filename='', level=logging.INFO)
 logging.basicConfig(filename='', level=logging.DEBUG,
                     format='%(asctime)s:%(levelname)s:%(message)s')
 
+
+class raspberry_post_server(socketserver.StreamRequestHandler):
+    def handle(self):
+        global buzzer_state
+
+        client = self.request.getpeername()
+        logging.info("Client connecting: {}".format(client))
+
+        for line in self.rfile:
+            # get a request message in JSON and converts into dict
+            try:
+                request = json.loads(line.decode('utf-8'))
+            except ValueError as e:
+                # reply ERROR response message
+                error_msg = '{}: json decoding error'.format(e)
+                status = 'ERROR {}'.format(error_msg)
+                response = dict(status=status, deviceid=request.get('deviceid'),
+                                msgid=request.get('msgid'))
+                response = json.dumps(response)
+                self.wfile.write(response.encode('utf-8') + b'\n')
+                self.wfile.flush()
+                logging.error(error_msg)
+                break
+            else:
+                status = 'OK'
+                logging.debug("{}:{}".format(client, request))
+
+
+            # reply response message
+            response = dict(status=status, deviceid=request.get('deviceid'),
+                            msgid=request.get('msgid'))
+            activate = {}
+            activate["buzzer"] = buzzer_state
+            if activate:
+                response['activate'] = activate
+            response = json.dumps(response)
+            self.wfile.write(response.encode('utf-8') + b'\n')
+            self.wfile.flush()
+            logging.debug("%s" % response)
+
+        # end of for loop
+        logging.info('Client closing: {}'.format(client))
+
+
 def rssi_renew_thread():
     rssi_addr = ("", 8090)
-    with socketserver.ThreadingTCPServer(rssi_addr, IoTRequestHandler) as server:
+    with socketserver.ThreadingTCPServer(rssi_addr, rssiRenew) as server:
         logging.info('rssi Server starts: {}'.format(rssi_addr))
+        server.serve_forever()
+
+
+def raspberry_post():
+    rasp_addr = ("", 8095)
+    with socketserver.ThreadingTCPServer(rasp_addr, raspberry_post_server) as server:
+        logging.info('rasp Server starts: {}'.format(rasp_addr))
         server.serve_forever()
 
 d = threading.Thread(name='rssi', target=rssi_renew_thread)
 d.start()
+
+e = threading.Thread(name='rasp', target=raspberry_post)
+e.start()
 
 serv_addr = ("", 10007)
 with socketserver.ThreadingTCPServer(serv_addr, IoTRequestHandler) as server:
--- backend/utils/raspberry.py
+++ backend/utils/raspberry.py
@@ -1,60 +1,128 @@
-import socketserver, json
-import logging, time
+"""
+We define IoT Protocol messages as Python dicts for example.
+They are serialized as JSON format string, then encoded in utf-8.
+Caution: because every messages are delimited by new line character (b'\n') in a TCP session,
+avoid using LF character inside Python strings.
 
-class IoTRequestHandler(socketserver.StreamRequestHandler):
-    def handle(self):
-        client = self.request.getpeername()
-        logging.info("Client connecting: {}".format(client))
+The POST request messages may be sent periodically
+for server to inform the client to activate the actuators if needed.
 
-        for line in self.rfile:
-            # get a request message in JSON and converts into dict
+<request message> ::= <request object in JSON format with UTF-8 encoding> <LF>
+
+<request object> ::=
+    {   'method': 'POST',
+        'deviceid': <device id>,
+        'msgid': <messge id>,
+        'data': {'temperature': 28.5, 'humidity': 71},
+    }
+
+<response message> ::= <response object in JSON format with UTF-8 encoding> <LF>
+
+<response object> ::=
+    {   'status': 'OK' | 'ERROR <error msg>',
+        'deviceid': <device id>
+        'msgid': <messge id>
+      [ 'activate': {'aircon': 'ON', 'led': 'OFF' } ]  # optional
+    }
+
+<LF> ::= b'\n'
+"""
+
+import socket, json, time, sys, socketserver
+import selectors, uuid
+import random, math
+import threading
+import logging
+
+# print uuid.uuid4()
+
+
+
+class IoTClient:
+    def __init__(self, server_addr, deviceid):
+        """IoT client with persistent connection
+        Each message separated by b'\n'
+
+        :param server_addr: (host, port)
+        :param deviceid: id of this IoT
+        """
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(server_addr)  # connect to server process
+        rfile = sock.makefile('rb')  # file-like obj
+        sel = selectors.DefaultSelector()
+        sel.register(sock, selectors.EVENT_READ)
+
+        self.sock = sock
+        self.rfile = rfile
+        self.deviceid = deviceid
+        self.sel = sel
+        self.requests = {}      # messages sent but not yet received their responses
+        self.time_to_expire = None
+
+    def select_periodic(self, interval):
+        """Wait for ready events or time interval.
+        Timeout event([]) occurs every interval, periodically.
+        """
+        now = time.time()
+        if self.time_to_expire is None:
+            self.time_to_expire = now + interval
+        timeout_left = self.time_to_expire - now
+        if timeout_left > 0:
+            events = self.sel.select(timeout=timeout_left)
+            if events:
+                return events
+        # time to expire elapsed or timeout event occurs
+        self.time_to_expire += interval  # set next time to expire
+        return []
+
+    def run(self):
+        msgid = 0
+
+        while True:
             try:
-                request = json.loads(line.decode('utf-8'))
-            except ValueError as e:
-                # reply ERROR response message
-                error_msg = '{}: json decoding error'.format(e)
-                status = 'ERROR {}'.format(error_msg)
-                response = dict(status=status, deviceid=request.get('deviceid'),
-                                msgid=request.get('msgid'))
-                response = json.dumps(response)
-                self.wfile.write(response.encode('utf-8') + b'\n')
-                self.wfile.flush()
-                logging.error(error_msg)
+                events = self.select_periodic(interval=5)
+                if not events:      # timeout occurs
+                    # msgid = str(uuid.uuid1())
+                    msgid += 1
+                    request = dict(method='POST', deviceid=self.deviceid, msgid=msgid, data="")
+                    logging.debug(request)
+                    request_bytes = json.dumps(request).encode('utf-8') + b'\n'
+                    self.sock.sendall(request_bytes)
+                    self.requests[msgid] = request_bytes
+                else:               # EVENT_READ
+                    response_bytes = self.rfile.readline()     # receive response
+                    if not response_bytes:
+                        self.sock.close()
+                        raise OSError('Server abnormally terminated')
+                    response = json.loads(response_bytes.decode('utf-8'))
+                    logging.debug(response)
+
+                    # msgid in response allows to identify the specific request message
+                    # It enables asynchronous transmission of request messages in pipelining
+                    msgid = response.get('msgid')
+                    buzzer_state = response.get('buzzer_state')
+                    if buzzer_state == "ON":
+                        print("on")
+                    elif buzzer_state == "OFF":
+                        print("off")
+
+                    if msgid and msgid in self.requests:
+                        del self.requests[msgid]
+                    else:
+                        logging.warning('{}: illegal msgid received. Ignored'.format(msgid))
+            except Exception as e:
+                logging.error(e)
                 break
-            else:
-                status = 'OK'
-                logging.debug("{}:{}".format(client, request))
+        # end of while loop
 
-            # extract the sensor data from the request
-            data = request.get('data')
-            if data:        # data exists
-                buzzer = data.get('buzzer')
+        logging.info('client terminated')
+        self.sock.close()
 
-            # Insert sensor data into DB tables
-            # and retrieve information to control the actuators
-            pass
-            print(buzzer)
 
-            # buzzer on
+if __name__ == '__main__':
 
-            # reply response message
-            response = dict(status=status, deviceid=request.get('deviceid'),
-                            msgid=request.get('msgid'))
-            
-            response = json.dumps(response)
-            self.wfile.write(response.encode('utf-8') + b'\n')
-            self.wfile.flush()
-            logging.debug("%s" % response)
-
-        # end of for loop
-        logging.info('Client closing: {}'.format(client))
-
-# logging.basicConfig(filename='', level=logging.INFO)
-logging.basicConfig(filename='', level=logging.DEBUG,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
-
-serv_addr = ("ec2-13-125-236-248.ap-northeast-2.compute.amazonaws.com", 8095)
-with socketserver.ThreadingTCPServer(serv_addr, IoTRequestHandler) as server:
-    logging.info('Server starts: {}'.format(serv_addr))
-    server.serve_forever()
-
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s:%(levelname)s:%(message)s')
+    client = IoTClient(("127.0.0.1", 8095), 2323)
+    client.run()
