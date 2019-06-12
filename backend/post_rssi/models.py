2fb141f22eec1b5cc038968d333c444cdf4a41d4
hochan222 <201502766@hufs.ac.kr>
2019-06-12 21:30:00 +0900

    asd

--- backend/post_rssi/models.py
+++ backend/post_rssi/models.py
@@ -2,15 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post_rssi(models.Model):
-    beaconAddress = models.CharField(max_length=200)
-    beaconType = models.CharField(max_length=200)
-    distance = models.IntegerField()
-    hashcode = models.CharField(max_length=200)
-    ibeaconData = models.CharField(max_length=200)
-    lastMinuSeen = models.IntegerField()
-    rssi = models.IntegerField()
-    txPower = models.IntegerField()
-    reader = models.CharField(max_length=200)
+    data = models.CharField(max_length=200)
 
     def __str__(self):
         return self.reader
