b75369b8589964c15af9bca667aea374eedeaaad
hochan222 <201502766@hufs.ac.kr>
2019-06-13 02:34:09 +0900

    sad

--- backend/post_rssi/models.py
+++ backend/post_rssi/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Post_rssi(models.Model):
-    data = models.CharField(max_length=200)
     distance = models.IntegerField()
 
     def __str__(self):
