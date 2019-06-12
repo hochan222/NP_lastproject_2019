397d66f7a4718b3548f59debc03318e2a6b38159
hochan222 <201502766@hufs.ac.kr>
2019-06-13 01:18:09 +0900

    sda

--- backend/utils/slack.py
+++ backend/utils/slack.py
@@ -5,18 +5,17 @@ import os, json
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TOKEN = []
 
-# secret_file = os.path.join(BASE_DIR, 'secret', 'slack_token.json')
+secret_file = os.path.join(BASE_DIR, 'secret', 'slack_token.json')
 
-# with open(secret_file) as f:
-#     secrets = json.loads(f.read())
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
 
-# def get_secret(setting, secrets=secrets):
-#     """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
-#     return secrets["slackToken"]
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    return secrets["slackToken"]
 
 def slack_notify(text=None, channel='#backend', username='알림봇', attachments=None):
-    # TOKEN = get_secret("slackToken")
-    TOKEN = "xoxb-623136227107-661697401447-ct3yujlEtdFnRCLwK8jP2Lcz"
+    TOKEN = get_secret("slackToken")
     slack = Slacker(TOKEN)
     attachments = [{
     "pretext": "경고 알림",
