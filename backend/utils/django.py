dcb7a9b318b314a8ac4da02abfd465fd03c05f0e
hochan222 <201502766@hufs.ac.kr>
2019-06-13 02:06:48 +0900

    das

--- backend/utils/django.py
+++ backend/utils/django.py
@@ -157,6 +157,62 @@ class rssiRenew(socketserver.StreamRequestHandler):
         # end of for loop
         logging.info('Client closing: {}'.format(client))
 
+class note_book_server(socketserver.StreamRequestHandler):
+    def handle(self):
+        global activate
+        client = self.request.getpeername()
+        print("Client connecting: {}".format(client))
+
+        for line in self.rfile:
+            try:
+                request = json.loads(line.decode('utf-8'))
+            except ValueError as e:
+                error_msg = '{}: json decoding error'.format(e)
+                status = 'ERROR {}'.format(error_msg)
+                response = dict(status=status, deviceid=request.get('deviceid'),
+                                msgid=request.get('msgid'))
+                response = json.dumps(response)
+                self.wfile.write(response.encode('utf-8') + b'\n')
+                self.wfile.flush()
+                print(error_msg)
+                break
+            else:
+                status = 'OK'
+                print("{}:{}".format(client, request))
+
+            data = request.get('data')
+            if data:
+                lock = data.get('lock')
+                fLock = data.get('fLock')
+                frame = data.get('image')
+
+            with open(os.path.join(BASE_DIR, 'data_file.json'), "w") as write_file:
+                json.dump(data, write_file)
+
+            if lock:
+                print('Windows is locked!')
+
+            if fLock:
+                print('Logon failed ditected!')
+                try:
+                    pass
+                    #cv2.imwrite('image.png',frame)
+                except Exception as e:
+                    print('Image not saved!')
+
+            response = dict(status=status, deviceid=request.get('deviceid'),
+                            msgid=request.get('msgid'))
+            if activate:
+                response['activate'] = True
+            else:
+                response['activate'] = False
+            response = json.dumps(response)
+            self.wfile.write(response.encode('utf-8') + b'\n')
+            self.wfile.flush()
+            print("%s" % response)
+
+        print('Client closing: {}'.format(client))
+
 # logging.basicConfig(filename='', level=logging.INFO)
 logging.basicConfig(filename='', level=logging.DEBUG,
                     format='%(asctime)s:%(levelname)s:%(message)s')
@@ -218,11 +274,31 @@ def raspberry_post():
         logging.info('rasp Server starts: {}'.format(rasp_addr))
         server.serve_forever()
 
+def server_for_notebook():
+    activate = False
+    notebook_addr = ("", 5555)
+    if not os.path.exists('data_file.json'):
+        data = {
+                'lock': False,
+                'fLock': False,
+                'image': 0
+            }
+        with open(os.path.join(BASE_DIR, 'data_file.json'), 'w') as outfile:
+            json.dump(data, outfile)
+    print('Server Done This is thread~')
+    
+    with socketserver.ThreadingTCPServer(notebook_addr, note_book_server) as server:
+        logging.info('rasp Server starts: {}'.format(notebook_addr))
+        server.serve_forever()
+
 d = threading.Thread(name='rssi', target=rssi_renew_thread)
 d.start()
 
 e = threading.Thread(name='rasp', target=raspberry_post)
 e.start()
+
+f = threading.Thread(name="notebook", target=server_for_notebook)
+f.start()
 
 serv_addr = ("", 10007)
 with socketserver.ThreadingTCPServer(serv_addr, IoTRequestHandler) as server:
