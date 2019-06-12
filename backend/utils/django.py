b77368b78a1e60e628a6f5f6145f3c0919ee9579
hochan222 <201502766@hufs.ac.kr>
2019-06-13 00:13:36 +0900

    sda

--- backend/utils/django.py
+++ backend/utils/django.py
@@ -16,7 +16,6 @@ with open(secret_file) as f:
     secrets = json.loads(f.read())
 
 def get_secret(setting, secrets=secrets):
-    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
     try:
         return secrets["slackToken"]
     except KeyError:
